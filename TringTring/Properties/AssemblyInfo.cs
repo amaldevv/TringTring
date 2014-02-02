613490b284dc9e8c3e7195075271cec589bc75aa
Amal <amaldevv@outlook.com>
2014-02-02 10:54:55 +0530

    design

--- TringTring/Properties/AssemblyInfo.cs
+++ TringTring/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Resources;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("04b53bfd-9263-4a30-804f-22760d83a028")]
+[assembly: Guid("189300e1-315a-47f0-80e8-4783c203ea01")]
 
 // Version information for an assembly consists of the following four values:
 //
